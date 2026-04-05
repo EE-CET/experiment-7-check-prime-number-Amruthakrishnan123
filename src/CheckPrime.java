ab9bdb38c94c3c8a7bfadd97c2847c4d096309fe
Amruthakrishnan123 <amruthakrishnan936@gmail.com>
2026-04-05 11:23:43 +0530

    exp7.done

--- src/CheckPrime.java
+++ src/CheckPrime.java
@@ -4,34 +4,35 @@ public class CheckPrime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // TODO: Read n
-        if (!scanner.hasNextInt()) return; // Safety check for valid input
-        int n = scanner.nextInt();
-        
-        // TODO: Check if n is prime
-        boolean isPrime = true;
-
-        // Hint 1: If n <= 1, it is Not Prime.
-        if (n <= 1) {
-            isPrime = false;
-        } else {
-            // Hint 2: Loop from 2 to sqrt(n). 
-            // If n is divisible by any number, it is Not Prime.
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break; // Exit loop early if a divisor is found
-                }
+        // Check if there is an integer to read to avoid errors
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            
+            if (isPrime(n)) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not Prime");
             }
-        }
-
-        // Hint 3 & TODO: Print "Prime" or "Not Prime"
-        if (isPrime) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Not Prime");
         }
         
         scanner.close();
     }
-}1
+
+    public static boolean isPrime(int n) {
+        // 1. If n <= 1, it is Not Prime.
+        if (n <= 1) {
+            return false;
+        }
+        
+        // 2. Loop from 2 to sqrt(n).
+        // If n is divisible by any number, it is Not Prime.
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        
+        // 3. Otherwise, it is Prime.
+        return true;
+    }
+}
